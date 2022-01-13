fff163544a39800b569ae15942900d482a6817b6
John Morgan <johnpatrickmorganuk@gmail.com>
2022-01-13 22:07:20 +0000

    Pins FlowStacks dependency to ... 0.0.14

--- Package.swift
+++ Package.swift
@@ -3,28 +3,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "TCACoordinators",
-    platforms: [
-        .iOS(.v13), .watchOS(.v7), .macOS(.v11), .tvOS(.v13),
-    ],
-    products: [
-        .library(
-            name: "TCACoordinators",
-            targets: ["TCACoordinators"]),
-    ],
-    dependencies: [
-         .package(url: "https://github.com/johnpatrickmorgan/FlowStacks", from: "0.0.13"),
-         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.27.1"),
-    ],
-    targets: [
-        .target(
-            name: "TCACoordinators",
-            dependencies: [
-                .product(name: "FlowStacks", package: "FlowStacks"),
-                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
-            ]),
-        .testTarget(
-            name: "TCACoordinatorsTests",
-            dependencies: ["TCACoordinators"]),
-    ]
-)
+  name: "TCACoordinators",
+  platforms: [
+    .iOS(.v13), .watchOS(.v7), .macOS(.v11), .tvOS(.v13),
+  ],
+  products: [
+    .library(
+      name: "TCACoordinators",
+      targets: ["TCACoordinators"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/johnpatrickmorgan/FlowStacks", "0.0.10" ... "0.0.14"),
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.27.1"),
+  ],
+  targets: [
+    .target(
+      name: "TCACoordinators",
+      dependencies: [
+        .product(name: "FlowStacks", package: "FlowStacks"),
+        .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+      ]),
+    .testTarget(
+      name: "TCACoordinatorsTests",
+      dependencies: ["TCACoordinators"]),
+  ])
