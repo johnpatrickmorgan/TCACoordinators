cb82c47c3d2311c89800256f21305f504fb555f4
Rhys Morgan <rhysm@me.com>
2023-07-31 20:07:14 +0100

    Update to official Composable Architecture 1.0 release!

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/johnpatrickmorgan/FlowStacks", from: "0.3.0"),
-    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", .branch("release/1.0")),
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.0.0"),
   ],
   targets: [
     .target(
