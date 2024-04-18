1b73c61191701ec351e5c9d7ff74ad6ad4e926aa
Rhys Morgan <rhysm@me.com>
2024-04-18 10:12:28 +0100

    Re-adds comments to the updateRoutes and routeAction cases

--- Sources/TCACoordinators/Protocols/IdentifiedRouterAction.swift
+++ Sources/TCACoordinators/Protocols/IdentifiedRouterAction.swift
@@ -3,25 +3,11 @@ import ComposableArchitecture
 import FlowStacks
 import Foundation
 
-///// A protocol standardizing naming conventions for action types that can manage routes
-///// within an `IdentifiedArray`.
-//public protocol IdentifiedRouterAction {
-//  associatedtype Screen: Identifiable
-//  associatedtype ScreenAction
-//
-//  /// Creates an action that allows routes to be updated whenever the user navigates back.
-//  /// - Returns: An `IdentifiedRouteAction`, usually a case in an enum.
-//  static func updateRoutes(_ routes: IdentifiedArrayOf<Route<Screen>>) -> Self
-//
-//  /// Creates an action that allows a child action for a specific screen to be dispatched to that
-//  /// screen's reducer.
-//  /// - Returns: An `IdentifiedRouteCoordinatorAction`, usually a case in an enum.
-//  static func routeAction(_ id: Screen.ID, action: ScreenAction) -> Self
-//}
-//
-
 @CasePathable
 public enum IdentifiedRouterAction<Screen: Identifiable, ScreenAction> {
+	/// An action that allows routes to be updated whenever the user navigates back.
 	case updateRoutes(_ routes: IdentifiedArrayOf<Route<Screen>>)
+
+	/// An action that allows a child action for a specific screen to be dispatched to that screen's reducer
 	case routeAction(IdentifiedAction<Screen.ID, ScreenAction>)
 }
--- Sources/TCACoordinators/Protocols/IndexedRouterAction.swift
+++ Sources/TCACoordinators/Protocols/IndexedRouterAction.swift
@@ -3,25 +3,11 @@ import ComposableArchitecture
 import FlowStacks
 import Foundation
 
-/// A protocol standardizing naming conventions for action types that can manage routes
-/// within an `Array`.
-//public protocol IndexedRouterAction {
-//  associatedtype Screen
-//  associatedtype ScreenAction
-//
-//  /// Creates an action that allows routes to be updated whenever the user navigates back.
-//  /// - Returns: An `IndexedScreenCoordinatorAction`, usually a case in an enum.
-//  static func updateRoutes(_ screens: [Route<Screen>]) -> Self
-//	static var updateRoutesCKP: CaseKeyPath<Self, [Route<Screen>]> { get }
-//
-//  /// Creates an action that allows the action for a specific screen to be dispatched to that screen's reducer.
-//  /// - Returns: An `IndexedScreenCoordinatorAction`, usually a case in an enum.
-//  static func routeAction(_ index: Int, action: ScreenAction) -> Self
-//	static var routeActionCKP: CaseKeyPath<Self, (Int, action: ScreenAction)> { get }
-//}
-
 @CasePathable
 public enum IndexedRouterAction<Screen, ScreenAction> {
+	/// An action that allows routes to be updated whenever the user navigates back.
 	case updateRoutes(_ routes: [Route<Screen>])
+
+	/// An action that allows the action for a specific screen to be dispatched to that screen's reducer.
 	case routeAction(IdentifiedAction<Int, ScreenAction>)
 }
