9ae03cad0d3df261e9e7af2cea8cd862f1b12bb7
Rhys Morgan <rhysm@me.com>
2024-04-18 19:31:22 +0100

    Rewrites IndexedRouterAction as a typealias of RouterAction with an Int ID

--- Sources/TCACoordinators/Protocols/IdentifiedRouterAction.swift
+++ Sources/TCACoordinators/Protocols/IdentifiedRouterAction.swift
@@ -11,3 +11,11 @@ public enum IdentifiedRouterAction<Screen: Identifiable, ScreenAction> {
 	/// An action that allows a child action for a specific screen to be dispatched to that screen's reducer
 	case routeAction(IdentifiedAction<Screen.ID, ScreenAction>)
 }
+
+@CasePathable
+public enum RouterAction<Screen, ID: Hashable, ScreenAction> {
+	case updateRoutes(_ routes: [Route<Screen>])
+	case routeAction(IdentifiedAction<ID, ScreenAction>)
+}
+
+public typealias _IdentifiedRouterAction<Screen, ScreenAction> = RouterAction<Screen, Screen.ID, ScreenAction> where Screen: Identifiable
--- Sources/TCACoordinators/Protocols/IndexedRouterAction.swift
+++ Sources/TCACoordinators/Protocols/IndexedRouterAction.swift
@@ -1,13 +1,1 @@
-import CasePaths
-import ComposableArchitecture
-import FlowStacks
-import Foundation
-
-@CasePathable
-public enum IndexedRouterAction<Screen, ScreenAction> {
-	/// An action that allows routes to be updated whenever the user navigates back.
-	case updateRoutes(_ routes: [Route<Screen>])
-
-	/// An action that allows the action for a specific screen to be dispatched to that screen's reducer.
-	case routeAction(IdentifiedAction<Int, ScreenAction>)
-}
+public typealias IndexedRouterAction<Screen, ScreenAction> = RouterAction<Screen, Int, ScreenAction>
