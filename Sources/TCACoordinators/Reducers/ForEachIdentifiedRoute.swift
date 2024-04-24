f175ba812dbcc94afa13006a7b0846140e3313f5
Rhys Morgan <rhysm@me.com>
2024-04-24 21:58:21 +0100

    Updates some generics in ForEachIdentified/IndexedRoute

--- Sources/TCACoordinators/Reducers/ForEachIdentifiedRoute.swift
+++ Sources/TCACoordinators/Reducers/ForEachIdentifiedRoute.swift
@@ -2,32 +2,32 @@ import ComposableArchitecture
 import Foundation
 
 struct ForEachIdentifiedRoute<
-	CoordinatorReducer: Reducer,
-	ScreenReducer: Reducer,
-	CoordinatorID: Hashable
+  CoordinatorReducer: Reducer,
+  ScreenReducer: Reducer,
+  CoordinatorID: Hashable
 >: Reducer
-where ScreenReducer.State: Identifiable,
-			CoordinatorReducer.Action: CasePathable,
-			ScreenReducer.Action: CasePathable
+  where ScreenReducer.State: Identifiable,
+  CoordinatorReducer.Action: CasePathable,
+  ScreenReducer.Action: CasePathable
 {
   let coordinatorReducer: CoordinatorReducer
   let screenReducer: ScreenReducer
   let cancellationId: CoordinatorID?
   let toLocalState: WritableKeyPath<CoordinatorReducer.State, IdentifiedArrayOf<Route<ScreenReducer.State>>>
-	let toLocalAction: CaseKeyPath<CoordinatorReducer.Action, IdentifiedRouterAction<ScreenReducer.State, ScreenReducer.Action>>
+  let toLocalAction: CaseKeyPath<CoordinatorReducer.Action, IdentifiedRouterAction<ScreenReducer.State, ScreenReducer.Action>>
 
   var body: some ReducerOf<CoordinatorReducer> {
     CancelEffectsOnDismiss(
       coordinatedScreensReducer: EmptyReducer()
-				.forEach(toLocalState, action: toLocalAction.appending(path: \.routeAction).appending(path: \.[])) {
+        .forEach(toLocalState, action: toLocalAction.appending(path: \.routeAction).appending(path: \.[])) {
           OnRoutes(wrapped: screenReducer)
         }
         .updatingRoutesOnInteraction(
-					updateRoutes: toLocalAction.appending(path: \.updateRoutes).appending(path: \.[]),
-					toLocalState: toLocalState
+          updateRoutes: toLocalAction.appending(path: \.updateRoutes).appending(path: \.[]),
+          toLocalState: toLocalState
         ),
-			routes: toLocalState,
-			routeAction: toLocalAction.appending(path: \.routeAction),
+      routes: toLocalState,
+      routeAction: toLocalAction.appending(path: \.routeAction),
       cancellationId: cancellationId,
       getIdentifier: { element, _ in element.id },
       coordinatorReducer: coordinatorReducer
@@ -36,17 +36,17 @@ where ScreenReducer.State: Identifiable,
 }
 
 public extension Reducer {
-  func forEachRoute<ScreenReducer: Reducer, ScreenState, ScreenAction, CoordinatorID: Hashable>(
-    cancellationId: CoordinatorID?,
-    toLocalState: WritableKeyPath<Self.State, IdentifiedArrayOf<Route<ScreenReducer.State>>>,
-		toLocalAction: CaseKeyPath<Self.Action, IdentifiedRouterAction<ScreenReducer.State, ScreenReducer.Action>>,
+  func forEachRoute<ScreenReducer: Reducer, ScreenState, ScreenAction>(
+    cancellationId: (some Hashable)?,
+    toLocalState: WritableKeyPath<Self.State, IdentifiedArrayOf<Route<ScreenState>>>,
+    toLocalAction: CaseKeyPath<Self.Action, IdentifiedRouterAction<ScreenState, ScreenAction>>,
     @ReducerBuilder<ScreenState, ScreenAction> screenReducer: () -> ScreenReducer
   ) -> some ReducerOf<Self>
-	where Action: CasePathable,
-				ScreenReducer.State: Identifiable,
-        ScreenState == ScreenReducer.State,
-        ScreenAction == ScreenReducer.Action,
-				ScreenAction: CasePathable
+    where Action: CasePathable,
+    ScreenReducer.State: Identifiable,
+    ScreenState == ScreenReducer.State,
+    ScreenAction == ScreenReducer.Action,
+    ScreenAction: CasePathable
   {
     ForEachIdentifiedRoute(
       coordinatorReducer: self,
@@ -57,60 +57,60 @@ public extension Reducer {
     )
   }
 
-	/// Allows a screen reducer to be incorporated into a coordinator reducer, such that each screen in
-	/// the coordinator's routes IdentifiedArray will have its actions and state propagated. When screens are
-	/// dismissed, the routes will be updated. If a cancellation identifier is passed, in-flight effects
-	/// will be cancelled when the screen from which they originated is dismissed.
-	/// - Parameters:
-	///   - cancellationIdType: A type to use for cancelling in-flight effects when a view is dismissed. It
-	///   will be combined with the screen's identifier. Defaults to the type of the parent reducer.
-	///   - screenReducer: The reducer that operates on all of the individual screens.
-	/// - Returns: A new reducer combining the coordinator-level and screen-level reducers.
-	func forEachRoute<ScreenReducer: Reducer, ScreenState, ScreenAction>(
-		cancellationIdType: Any.Type = Self.self,
-		_ routes: WritableKeyPath<State, IdentifiedArrayOf<Route<ScreenState>>>,
-		action: CaseKeyPath<Action, IdentifiedRouterAction<ScreenState, ScreenAction>>,
-		@ReducerBuilder<ScreenState, ScreenAction> screenReducer: () -> ScreenReducer
-	) -> some ReducerOf<Self>
-	where ScreenReducer.State: Identifiable,
-				Action: CasePathable,
-				ScreenState == ScreenReducer.State,
-				ScreenAction == ScreenReducer.Action,
-				ScreenAction: CasePathable
-	{
-		ForEachIdentifiedRoute(
-			coordinatorReducer: self,
-			screenReducer: screenReducer(),
-			cancellationId: ObjectIdentifier(cancellationIdType),
-			toLocalState: routes,
-			toLocalAction: action
-		)
-	}
+  /// Allows a screen reducer to be incorporated into a coordinator reducer, such that each screen in
+  /// the coordinator's routes IdentifiedArray will have its actions and state propagated. When screens are
+  /// dismissed, the routes will be updated. If a cancellation identifier is passed, in-flight effects
+  /// will be cancelled when the screen from which they originated is dismissed.
+  /// - Parameters:
+  ///   - cancellationIdType: A type to use for cancelling in-flight effects when a view is dismissed. It
+  ///   will be combined with the screen's identifier. Defaults to the type of the parent reducer.
+  ///   - screenReducer: The reducer that operates on all of the individual screens.
+  /// - Returns: A new reducer combining the coordinator-level and screen-level reducers.
+  func forEachRoute<ScreenReducer: Reducer, ScreenState, ScreenAction>(
+    cancellationIdType: Any.Type = Self.self,
+    _ routes: WritableKeyPath<State, IdentifiedArrayOf<Route<ScreenState>>>,
+    action: CaseKeyPath<Action, IdentifiedRouterAction<ScreenState, ScreenAction>>,
+    @ReducerBuilder<ScreenState, ScreenAction> screenReducer: () -> ScreenReducer
+  ) -> some ReducerOf<Self>
+    where ScreenReducer.State: Identifiable,
+    Action: CasePathable,
+    ScreenState == ScreenReducer.State,
+    ScreenAction == ScreenReducer.Action,
+    ScreenAction: CasePathable
+  {
+    ForEachIdentifiedRoute(
+      coordinatorReducer: self,
+      screenReducer: screenReducer(),
+      cancellationId: ObjectIdentifier(cancellationIdType),
+      toLocalState: routes,
+      toLocalAction: action
+    )
+  }
 }
 
 extension Case {
-	subscript<Element: Identifiable>() -> Case<IdentifiedArray<Element.ID, Element>> where Value == [Element] {
-		Case<IdentifiedArrayOf<Element>>(
-			embed: { self.embed($0.elements) },
-			extract: {
-				self.extract(from: $0).flatMap { IdentifiedArrayOf(uniqueElements: $0) }
-			}
-		)
-	}
+  subscript<Element: Identifiable>() -> Case<IdentifiedArray<Element.ID, Element>> where Value == [Element] {
+    Case<IdentifiedArrayOf<Element>>(
+      embed: { self.embed($0.elements) },
+      extract: {
+        self.extract(from: $0).flatMap { IdentifiedArrayOf(uniqueElements: $0) }
+      }
+    )
+  }
 
-	fileprivate subscript<ID: Hashable, Action>() -> Case<IdentifiedAction<ID, Action>> where Value == (id: ID, action: Action) {
-		Case<IdentifiedAction<ID, Action>>(
-			embed: { action in
-				switch action {
-				case let .element(id, action):
-					self.embed((id, action))
-				}
-			},
-			extract: {
-				self.extract(from: $0).flatMap {
-					.element(id: $0, action: $1)
-				}
-			}
-		)
-	}
+  fileprivate subscript<ID: Hashable, Action>() -> Case<IdentifiedAction<ID, Action>> where Value == (id: ID, action: Action) {
+    Case<IdentifiedAction<ID, Action>>(
+      embed: { action in
+        switch action {
+        case let .element(id, action):
+          self.embed((id, action))
+        }
+      },
+      extract: {
+        self.extract(from: $0).flatMap {
+          .element(id: $0, action: $1)
+        }
+      }
+    )
+  }
 }
--- Sources/TCACoordinators/Reducers/ForEachIndexedRoute.swift
+++ Sources/TCACoordinators/Reducers/ForEachIndexedRoute.swift
@@ -2,85 +2,85 @@ import ComposableArchitecture
 import Foundation
 
 struct ForEachIndexedRoute<
-	CoordinatorReducer: Reducer,
-	ScreenReducer: Reducer,
-	CoordinatorID: Hashable
+  CoordinatorReducer: Reducer,
+  ScreenReducer: Reducer,
+  CoordinatorID: Hashable
 >: Reducer
-where CoordinatorReducer.Action: CasePathable,
-ScreenReducer.Action: CasePathable
+  where CoordinatorReducer.Action: CasePathable,
+  ScreenReducer.Action: CasePathable
 {
-	let coordinatorReducer: CoordinatorReducer
-	let screenReducer: ScreenReducer
-	let cancellationId: CoordinatorID?
-	let toLocalState: WritableKeyPath<CoordinatorReducer.State, [Route<ScreenReducer.State>]>
-	let toLocalAction: CaseKeyPath<CoordinatorReducer.Action, IndexedRouterAction<ScreenReducer.State, ScreenReducer.Action>>
+  let coordinatorReducer: CoordinatorReducer
+  let screenReducer: ScreenReducer
+  let cancellationId: CoordinatorID?
+  let toLocalState: WritableKeyPath<CoordinatorReducer.State, [Route<ScreenReducer.State>]>
+  let toLocalAction: CaseKeyPath<CoordinatorReducer.Action, IndexedRouterAction<ScreenReducer.State, ScreenReducer.Action>>
 
-	var body: some ReducerOf<CoordinatorReducer> {
-		CancelEffectsOnDismiss(
-			coordinatedScreensReducer: EmptyReducer()
-				.forEachIndex(toLocalState, action: toLocalAction.appending(path: \.routeAction)) {
-					OnRoutes(wrapped: screenReducer)
-				}
-				.updatingRoutesOnInteraction(
-					updateRoutes: toLocalAction.appending(path: \.updateRoutes),
-					toLocalState: toLocalState
-				),
-			routes: toLocalState,
-			routeAction: toLocalAction.appending(path: \.routeAction),
-			cancellationId: cancellationId,
-			getIdentifier: { _, index in index },
-			coordinatorReducer: coordinatorReducer
-		)
-	}
+  var body: some ReducerOf<CoordinatorReducer> {
+    CancelEffectsOnDismiss(
+      coordinatedScreensReducer: EmptyReducer()
+        .forEachIndex(toLocalState, action: toLocalAction.appending(path: \.routeAction)) {
+          OnRoutes(wrapped: screenReducer)
+        }
+        .updatingRoutesOnInteraction(
+          updateRoutes: toLocalAction.appending(path: \.updateRoutes),
+          toLocalState: toLocalState
+        ),
+      routes: toLocalState,
+      routeAction: toLocalAction.appending(path: \.routeAction),
+      cancellationId: cancellationId,
+      getIdentifier: { _, index in index },
+      coordinatorReducer: coordinatorReducer
+    )
+  }
 }
 
 public extension Reducer {
-	func forEachRoute<ScreenReducer: Reducer, ScreenState, ScreenAction, CoordinatorID: Hashable>(
-    coordinatorIdForCancellation: CoordinatorID?,
-    toLocalState: WritableKeyPath<Self.State, [Route<ScreenReducer.State>]>,
-		toLocalAction: CaseKeyPath<Self.Action, IndexedRouterAction<ScreenReducer.State, ScreenReducer.Action>>,
+  func forEachRoute<ScreenReducer: Reducer, ScreenState, ScreenAction>(
+    coordinatorIdForCancellation: (some Hashable)?,
+    toLocalState: WritableKeyPath<Self.State, [Route<ScreenState>]>,
+    toLocalAction: CaseKeyPath<Self.Action, IndexedRouterAction<ScreenState, ScreenAction>>,
     @ReducerBuilder<ScreenState, ScreenAction> screenReducer: () -> ScreenReducer
   ) -> some ReducerOf<Self>
-	where Action: CasePathable,
-				ScreenState == ScreenReducer.State,
-        ScreenAction == ScreenReducer.Action,
-				ScreenAction: CasePathable
+    where Action: CasePathable,
+    ScreenState == ScreenReducer.State,
+    ScreenAction == ScreenReducer.Action,
+    ScreenAction: CasePathable
   {
     ForEachIndexedRoute(
       coordinatorReducer: self,
       screenReducer: screenReducer(),
       cancellationId: coordinatorIdForCancellation,
       toLocalState: toLocalState,
-			toLocalAction: toLocalAction
+      toLocalAction: toLocalAction
     )
   }
 
-	/// Allows a screen reducer to be incorporated into a coordinator reducer, such that each screen in
-	/// the coordinator's routes Array will have its actions and state propagated. When screens are
-	/// dismissed, the routes will be updated. If a cancellation identifier is passed, in-flight effects
-	/// will be cancelled when the screen from which they originated is dismissed.
-	/// - Parameters:
-	///   - cancellationIdType: A type to use for cancelling in-flight effects when a view is dismissed. It
-	///   will be combined with the screen's identifier. Defaults to the type of the parent reducer.
-	///   - screenReducer: The reducer that operates on all of the individual screens.
-	/// - Returns: A new reducer combining the coordinator-level and screen-level reducers.
-	func forEachRoute<ScreenReducer: Reducer, ScreenState, ScreenAction>(
-		_ routes: WritableKeyPath<State, [Route<ScreenState>]>,
-		action: CaseKeyPath<Action, IndexedRouterAction<ScreenState, ScreenAction>>,
-		cancellationIdType: Any.Type = Self.self,
-		@ReducerBuilder<ScreenState, ScreenAction> screenReducer: () -> ScreenReducer
-	) -> some ReducerOf<Self>
-	where Action: CasePathable,
-				ScreenState == ScreenReducer.State,
-				ScreenAction == ScreenReducer.Action,
-				ScreenAction: CasePathable
-	{
-		ForEachIndexedRoute(
-			coordinatorReducer: self,
-			screenReducer: screenReducer(),
-			cancellationId: ObjectIdentifier(cancellationIdType),
-			toLocalState: routes,
-			toLocalAction: action
-		)
-	}
+  /// Allows a screen reducer to be incorporated into a coordinator reducer, such that each screen in
+  /// the coordinator's routes Array will have its actions and state propagated. When screens are
+  /// dismissed, the routes will be updated. If a cancellation identifier is passed, in-flight effects
+  /// will be cancelled when the screen from which they originated is dismissed.
+  /// - Parameters:
+  ///   - cancellationIdType: A type to use for cancelling in-flight effects when a view is dismissed. It
+  ///   will be combined with the screen's identifier. Defaults to the type of the parent reducer.
+  ///   - screenReducer: The reducer that operates on all of the individual screens.
+  /// - Returns: A new reducer combining the coordinator-level and screen-level reducers.
+  func forEachRoute<ScreenReducer: Reducer, ScreenState, ScreenAction>(
+    _ routes: WritableKeyPath<State, [Route<ScreenState>]>,
+    action: CaseKeyPath<Action, IndexedRouterAction<ScreenState, ScreenAction>>,
+    cancellationIdType: Any.Type = Self.self,
+    @ReducerBuilder<ScreenState, ScreenAction> screenReducer: () -> ScreenReducer
+  ) -> some ReducerOf<Self>
+    where Action: CasePathable,
+    ScreenState == ScreenReducer.State,
+    ScreenAction == ScreenReducer.Action,
+    ScreenAction: CasePathable
+  {
+    ForEachIndexedRoute(
+      coordinatorReducer: self,
+      screenReducer: screenReducer(),
+      cancellationId: ObjectIdentifier(cancellationIdType),
+      toLocalState: routes,
+      toLocalAction: action
+    )
+  }
 }
