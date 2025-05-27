ea1ce46da7850e614bc0828d045da51f7f01bc06
johnpatrickmorgan <johnpatrickmorganuk@gmail.com>
2025-05-27 21:58:50 +0100

    Adds conditional Hashable conformance to Route

---
+++ Sources/TCACoordinators/Route+Hashable.swift
@@ -0,0 +1,7 @@
+extension Route: @retroactive Hashable where Screen: Hashable {
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(style)
+    hasher.combine(embedInNavigationView)
+    hasher.combine(screen)
+  }
+}
