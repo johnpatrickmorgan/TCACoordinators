d56cc671b3a2105176a954326d4010f4cbaf4708
Roman Podymov <podymfrombryansk@yandex.com>
2026-04-06 23:24:29 +0200

    Copied platforms from TCA

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 let package = Package(
   name: "TCACoordinators",
   platforms: [
-    .iOS(.v14), .watchOS(.v7), .macOS(.v11), .tvOS(.v14),
+    .iOS(.v16),
+    .macOS(.v13),
+    .tvOS(.v16),
+    .watchOS(.v9),
   ],
   products: [
     .library(
